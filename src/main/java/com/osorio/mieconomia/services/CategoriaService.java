55f7b862f10f197146402a615ac8dfd1566ab1cb
Jose Osorio Catalan <jossekarlos10@gmail.com>
2023-01-29 13:50:22 -0500

    resolve conflict.

--- src/main/java/com/osorio/mieconomia/services/CategoriaService.java
+++ src/main/java/com/osorio/mieconomia/services/CategoriaService.java
@@ -4,12 +4,11 @@ package com.osorio.mieconomia.services;
 import com.osorio.developer.commons.models.Categoria;
 import com.osorio.mieconomia.repositories.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
+import java.util.List;
 
 
 @Service("CategoriaService")
